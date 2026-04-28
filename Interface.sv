8e602883519ce0791513bffc2f594172439361da
Ariel Topaz <130974714+Ariel424@users.noreply.github.com>
2026-04-28 10:27:31 +0300

    Update Interface.sv

--- Interface.sv
+++ Interface.sv
@@ -1,29 +1,39 @@
-interface my_interface (input logic clk); 
-  
-logic clk;
+interface my_interface #(parameter int DATA_WIDTH = 8) (input logic clk); 
+
 logic reset;
 logic write;
 logic read;
 logic empty;
 logic full;
-logic [7:0] data_in, data_out;
+logic [DATA_WIDTH - 1:0] data_in;
+logic [DATA_WIDTH - 1:0] data_out;
 
-clocking drv_cb @(posedge clk);
+clocking pro_cb @(posedge clk);
 default input #1ns output #1ns;
 output write;
 output data_in; 
 input full; 
 endclocking 
 
-clocking mon_cb @(posedge clk);
+clocking con_cb @(posedge clk);
 default input #1ns output #1ns;
 output read;
-output data_out; 
-input empry; 
+input data_out;
+input empty; 
+endclocking 
+  
+clocking mon_cb @(posedge clk);
+default input #1ns output #1ns;
+input write;
+input read;
+input empty; 
+input full;
+input data_in;
+input data_out;
 endclocking 
 
-modport DRIVER_MP (clocking drv_cb, input reset);
+modport PRODUCER_MP (clocking pro_cb, input reset);
+modport CONSUMER_MP (clocking con_cb, input reset);  
 modport MONITOR_MP (clocking mon_cb, input reset);  
     
 endinterface
-
