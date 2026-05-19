07051d406aa1e5848bfb2afd2ee1ea5ae786aba6
Ariel Topaz <130974714+Ariel424@users.noreply.github.com>
2026-05-19 16:53:25 +0300

    Update Interface.sv

--- Interface.sv
+++ Interface.sv
@@ -1,39 +1,38 @@
 interface my_interface #(parameter int DATA_WIDTH = 8) (input logic clk); 
 
-logic reset;
-logic write;
-logic read;
-logic empty;
-logic full;
-logic [DATA_WIDTH - 1:0] data_in;
-logic [DATA_WIDTH - 1:0] data_out;
+    logic reset;
+    logic write;
+    logic read;
+    logic empty;
+    logic full;
+    logic [DATA_WIDTH - 1:0] data_in;
+    logic [DATA_WIDTH - 1:0] data_out;
 
-clocking pro_cb @(posedge clk);
-default input #1ns output #1ns;
-output write;
-output data_in; 
-input full; 
-endclocking 
+    clocking pro_cb @(posedge clk);
+        default input #1ns output #1ns;
+        output write;
+        output data_in; 
+        input  full; 
+    endclocking 
 
-clocking con_cb @(posedge clk);
-default input #1ns output #1ns;
-output read;
-input data_out;
-input empty; 
-endclocking 
-  
-clocking mon_cb @(posedge clk);
-default input #1ns output #1ns;
-input write;
-input read;
-input empty; 
-input full;
-input data_in;
-input data_out;
-endclocking 
+    clocking con_cb @(posedge clk);
+        default input #1ns output #1ns;
+        output read;
+        input  data_out;
+        input  empty; 
+    endclocking 
+      
+    clocking mon_cb @(posedge clk);
+        default input #1ns output #1ns;
+        input write;
+        input read;
+        input empty; 
+        input full;
+        input data_in;
+        input data_out;
+    endclocking 
 
-modport PRODUCER_MP (clocking pro_cb, input reset);
-modport CONSUMER_MP (clocking con_cb, input reset);  
-modport MONITOR_MP (clocking mon_cb, input reset);  
-    
+    modport DRIVER_MP  (clocking pro_cb, clocking con_cb, output reset);
+    modport MONITOR_MP (clocking mon_cb, input reset);  
+        
 endinterface
